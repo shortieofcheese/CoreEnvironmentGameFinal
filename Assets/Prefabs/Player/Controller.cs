2db9ff60427dee4155c94af8d497faaa9f7e8e88
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-04-20 18:32:00 -0400

    Player logic.

---
+++ Assets/Prefabs/Player/Controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	GameObject normal, polaroid;
+	bool switched;
+
+
+	// Use this for initialization
+	void Start () {
+
+		normal = GameObject.Find("FirstPersonCharacter");
+		polaroid = GameObject.Find("Polaroid");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)){
+			if (!switched) {
+				normal.tag = "Untagged";
+				polaroid.tag = "MainCamera";
+				normal.GetComponent<Camera>().enabled = false;
+				polaroid.GetComponent<Camera>().enabled = true;
+				switched = !switched;
+				
+			} else {
+				normal.tag = "MainCamera";
+				polaroid.tag = "Untagged";
+				polaroid.GetComponent<Camera>().enabled = false;
+				normal.GetComponent<Camera>().enabled = true;
+				switched = !switched;
+				
+			}
+
+		}
+
+
+	
+	}
+}
---
+++ Assets/Prefabs/Player/auxCam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class auxCam : MonoBehaviour {
+
+	public GameObject thingToFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rotation = thingToFollow.transform.rotation;
+	
+	}
+}
