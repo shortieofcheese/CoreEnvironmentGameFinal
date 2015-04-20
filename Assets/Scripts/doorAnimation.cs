eb194ca45a253b615915d73697b4fcb70dc5d914
Danny of Cheese <shortieofcheese@gmail.com>
2015-04-20 16:59:07 -0400

    Things look good?

---
+++ Assets/Scripts/doorAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorAnimation : MonoBehaviour {
+	
+	public GameObject door;
+	public bool isOpen = false;
+
+	//Animation that opens and closes the home doors on mouse click.
+
+	void OnMouseDown(){
+		
+		if(!this.isOpen){
+			
+			door.GetComponent<Animation>().Play("door open");
+			
+			this.isOpen = true;
+			
+		}else{
+			door.GetComponent<Animation>().Play("door close");
+			
+			this.isOpen = false;
+			
+		}
+		
+	}
+
+}
+
---
+++ Assets/Scripts/nightStandAnimation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nightStandAnimation : MonoBehaviour {
+
+	public GameObject drawer;
+	public bool isOpen = false;
+
+	//Aninimation that opens and closes the nightstand drawer on mouse click.
+
+	void OnMouseDown(){
+
+		if(!this.isOpen){
+
+			drawer.GetComponent<Animation>().Play("Night Stand Open");
+
+			this.isOpen = true;
+
+		}else{
+			drawer.GetComponent<Animation>().Play("Night Stand Close");
+
+			this.isOpen = false;
+
+		}
+
+	}
+
+}
