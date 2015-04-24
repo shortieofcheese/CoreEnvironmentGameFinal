918e17b7df94ba8248cbce3a79b79eef6562c52c
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-04-24 14:48:32 -0400

    Added iTween plugin and created a Test Scene for it. That stuff is so neat!

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+			
+		iTween.MoveTo(this.gameObject, iTween.Hash ("path", iTweenPath.GetPath("Move"), "time", 20f));
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt(cube);
+
+	
+	}
+}
