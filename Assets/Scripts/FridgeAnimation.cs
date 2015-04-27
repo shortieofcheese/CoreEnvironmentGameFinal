b7e62dd5574b21845053b016700f026dda4d80ab
Ashley M Davis <davia966@newschool.edu>
2015-04-27 18:29:30 -0400

    Prefab stuff

---
+++ Assets/Scripts/FridgeAnimation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FridgeAnimation : MonoBehaviour {
+	
+	public GameObject fridge;
+	public bool isOpen = false;
+	bool mousePressed;
+	
+	void Update(){
+		
+		mousePressed = (Input.GetMouseButtonDown(0) ? true : false);
+		
+	}
+	
+	//Animation that opens and closes the home doors on mouse click.
+	void OnLook(){
+		
+		if (mousePressed){
+			
+			if(!this.isOpen){
+				
+				fridge.GetComponent<Animation>().Play("FridgeAnimationOPEN");
+				
+				this.isOpen = true;
+				
+			}else{
+				fridge.GetComponent<Animation>().Play("FridgeAnimationCLOSE");
+				
+				this.isOpen = false;
+				
+			}
+			
+		}
+		
+	}
+	
+}
