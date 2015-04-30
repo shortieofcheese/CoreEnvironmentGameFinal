464e01952a40ae5afeda10be8f7f20d6eb5249fd
Danny of Cheese <shortieofcheese@gmail.com>
2015-04-30 17:30:49 -0400

    UI canvas script

---
+++ Assets/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace UnityStandardAssets.Characters.FirstPerson
+{
+
+public class UIManager : MonoBehaviour {
+	
+	bool isLooking;
+	public GameObject canvasImage;
+
+	void Update () {
+		FirstPersonController player = GameObject.Find("FPSController").GetComponent<FirstPersonController>();
+
+		if(Input.GetMouseButtonDown(0)){
+				if(isLooking == true){
+					isLooking = false;
+				}else{
+					isLooking = true;
+				}
+		}
+			if(isLooking){
+				player.enabled = false;
+				canvasImage.SetActive(true);
+
+			}else{
+				player.enabled = true;
+				canvasImage.SetActive(false);
+
+			}
+
+
+
+	}
+}
+}
