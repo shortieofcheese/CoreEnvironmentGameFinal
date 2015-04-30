17bad1122f19039e4bce9f3265d32ff7b3a5ea73
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-04-30 11:41:52 -0400

    FadeIn/Out logic done, Dath Path scene are already teleporting to the working scene with fade.

---
+++ Assets/MakeFade.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MakeFade : MonoBehaviour {
+
+	public Image panel;
+	public float fadeDuration;
+
+	void Start(){
+		panel.CrossFadeAlpha(0f, 2f, false);
+	}
+
+	public void FadeInToScene(float d, int s){
+		StartCoroutine(FadeInToOtherScene(d,s));
+	}
+
+
+	IEnumerator FadeInToOtherScene (float duration, int scene){
+		panel.CrossFadeAlpha(1f, fadeDuration, false);
+		yield return new WaitForSeconds(duration + 0.5f);
+		Application.LoadLevel(scene);
+	}
+
+}
---
+++ Assets/checkTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkTrigger : MonoBehaviour {
+
+	MakeFade mf;
+	bool triggered;
+
+	// Use this for initialization
+	void Start () {
+		mf = GameObject.Find("_Game Manager").GetComponent<MakeFade>();	
+	}
+
+	void OnTriggerEnter (Collider obj){
+		if (obj.gameObject.tag == "Player" && !triggered){
+			mf.FadeInToScene(2.5f, 1);
+			triggered = true;
+		}
+	}
+
+}
