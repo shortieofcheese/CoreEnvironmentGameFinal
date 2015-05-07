1f59b5955d3164657e7f436a6a1761e5e2774e5d
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-05-07 17:42:29 -0400

    Objects events are working. Door animation fixed.

--- Assets/Scripts/doorAnimation.cs
+++ Assets/Scripts/doorAnimation.cs
@@ -7,34 +7,33 @@ public class doorAnimation : MonoBehaviour {
 	public bool isOpen = false;
 	bool mousePressed;
 	public bool lastDoor;
+	bool canAnimate;
 
-	void Update(){
-
-		mousePressed = (Input.GetMouseButtonDown(0) ? true : false);
-
+	void Start(){
+		canAnimate = true;
 	}
 
-	//Animation that opens and closes the home doors on mouse click.
+	void Update(){
+		mousePressed = (Input.GetMouseButtonDown(0) ? true : false);
+	}
+
 	void OnLook(){
-
-		if (mousePressed){
-
-			if(!this.isOpen && lastDoor){
-				
-				door.GetComponent<Animation>().Play("door open");
-				
-				this.isOpen = true;
-				
+		if (mousePressed && canAnimate){
+			StartCoroutine(letAnimation());
+			if(!this.isOpen){
+				door.GetComponent<Animation>().Play("door open");				
+				this.isOpen = true;				
 			}else if (!lastDoor){
 				door.GetComponent<Animation>().Play("door close");
-				
-				this.isOpen = false;
-				
+				this.isOpen = false;				
 			}
-
 		}
+	}
 
-		
+	IEnumerator letAnimation(){
+		canAnimate = false;
+		yield return new WaitForSeconds(.7f);
+		canAnimate = true;
 	}
 
 }
