360767e3a0a2cbe29590074d06f104e13c0806b2
Danny of Cheese <shortieofcheese@gmail.com>
2015-05-08 16:13:59 -0400

    LIGHT AND TEXTURES

---
+++ Assets/Lamp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lamp : MonoBehaviour {
+
+	public Material material1;
+	public Material material2;
+	public Light lampLight;
+	bool lightOn;
+
+	Renderer renderer;
+
+	void Start () {
+	
+		renderer = GetComponent<Renderer>();
+
+	}
+
+	void Update () {
+
+		if(lightOn == true){
+			renderer.material = material1;
+		}
+
+		if(lightOn == false){
+			renderer.material = material2;
+		}
+	
+	}
+	void OnLook(){
+		
+		if (Input.GetMouseButtonDown(0)){
+			if (lightOn){
+				
+				lampLight.enabled = false;
+				lightOn = false;
+			}else if (!lightOn){
+				lampLight.enabled = true;
+				lightOn = true;
+			}
+		}
+		
+	}
+
+}
