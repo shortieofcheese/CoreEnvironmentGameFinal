d0a872317f96f96d489ee8b1c79c755213191660
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-05-09 19:36:55 -0400

    Added asteroids to the floating house, camera animations, player can move when cutscene ends.

---
+++ Assets/Scripts/Asteroids/AsteroidMovementCamea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMovementCamea : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		iTween.MoveBy(this.gameObject, iTween.Hash("z" , -200f, "speed", .2f, "easetype", iTween.EaseType.linear));
+		iTween.RotateBy(this.gameObject, iTween.Hash("z", 1f, "time", 10f , "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
