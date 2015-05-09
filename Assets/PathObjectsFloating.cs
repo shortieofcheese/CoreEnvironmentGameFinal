6d0f47f597f424adf6dbb5a358405171c95dbdfe
Daniel Ribeiro <danielribeiro.new@gmail.com>
2015-05-09 17:39:57 -0400

    Objects on Path. They float and move up when you get close.

---
+++ Assets/PathObjectsFloating.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathObjectsFloating : MonoBehaviour {
+
+	public float yPositionAnimation;
+	public string objectName;
+	bool entered;
+
+	// Use this for initialization
+	void Start () {
+
+		objectName = this.gameObject.name;
+		iTween.MoveTo(this.gameObject, iTween.Hash("name", objectName, "y", yPositionAnimation, "time", 2f, "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong));
+
+	}
+
+	void OnTriggerEnter(Collider obj){
+		if (obj.gameObject.tag == "Player" && !entered){
+			entered = true;
+			iTween.StopByName(objectName);
+			iTween.MoveTo(this.gameObject, iTween.Hash("y", 6f, "time", 3f, "easetype", iTween.EaseType.easeInOutSine));
+		}
+	}
+	
+
+
+}
