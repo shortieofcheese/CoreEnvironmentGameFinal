3242756ebfbb5ead361372533141088b826e30c8
Danny of Cheese <shortieofcheese@gmail.com>
2015-05-09 17:19:08 -0400

    Fixed raycast and added paintbrush.

--- Assets/Scripts/Interaction Logic/RayCasting.cs
+++ Assets/Scripts/Interaction Logic/RayCasting.cs
@@ -12,7 +12,7 @@ public class RayCasting : MonoBehaviour {
 		Ray ray = new Ray( transform.position, transform.forward);
 		RaycastHit rayHit = new RaycastHit();
 
-		if ( Physics.Raycast ( ray, out rayHit, 1.1f, raycastMask ) ) {
+		if ( Physics.Raycast ( ray, out rayHit, 1.25f, raycastMask ) ) {
 			rayHit.transform.SendMessage ( functionToCallOnLook, SendMessageOptions.DontRequireReceiver );
 		}
 	}
