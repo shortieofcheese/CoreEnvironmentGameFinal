19bfb5d57b56b953c44a0d03780e4d8f9db96932
Danny of Cheese <shortieofcheese@gmail.com>
2015-05-14 01:53:28 -0400

    Light fix

--- Assets/Lamp.cs
+++ Assets/Lamp.cs
@@ -6,13 +6,14 @@ public class Lamp : MonoBehaviour {
 	public Material material1;
 	public Material material2;
 	public Light lampLight;
+	AudioSource audio;
 
 	bool lightOn;
 
 	new Renderer renderer;
 
 	void Start () {
-	
+		audio = GetComponent<AudioSource>();
 		renderer = GetComponent<Renderer>();
 
 	}
@@ -21,9 +22,11 @@ public class Lamp : MonoBehaviour {
 
 		if(lightOn == true){
 			renderer.material = material1;
+			//StartCoroutine (Beat());
 		}
 
 		if(lightOn == false){
+			//StopCoroutine(Beat());
 			renderer.material = material2;
 		}
 	
@@ -31,16 +34,29 @@ public class Lamp : MonoBehaviour {
 	void OnLook(){
 		
 		if (Input.GetMouseButtonDown(0)){
+
 			if (lightOn){
-				
+				StopCoroutine(Beat());
+				audio.Stop();
 				lampLight.enabled = false;
 				lightOn = false;
+
 			}else if (!lightOn){
+				StartCoroutine (Beat());
 				lampLight.enabled = true;
 				lightOn = true;
 			}
 		}
 		
+	}
+
+	IEnumerator Beat()
+	{
+			audio.Play();
+			yield return new WaitForSeconds(20.0f);
+			audio.Stop();
+			lightOn = false;
+
 	}
 
 }
